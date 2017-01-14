bd0a24f614960a5e07531af12035a2e49a303db8
MuChu Hsu <muchu1983@gmail.com>
2017-01-14 22:48:51 +0800

    remove route method style code

--- ciofant/core/consumers.py
+++ ciofant/core/consumers.py
@@ -6,40 +6,7 @@ This file is part of BSD license
 
 <https://opensource.org/licenses/BSD-3-Clause>
 """
-from channels import Group
-from channels.auth import channel_session_user
-from channels.auth import channel_session_user_from_http
 from channels.generic.websockets import JsonWebsocketConsumer
-import json
-
-# Connected to websocket.connect
-@channel_session_user_from_http
-def ws_connect(message, room):
-    # Accept connection
-    message.reply_channel.send({"accept": True})
-    # Save room in session and add us to the group
-    message.channel_session["room"] = room
-    Group("chat-%s" % room).add(message.reply_channel)
-
-# Connected to websocket.receive
-@channel_session_user
-def ws_message(message, room):
-    dicRespText = {
-        "data": message["text"],
-        "user": message.user.username,
-        "room": message.channel_session["room"]
-    }
-    Group("chat-%s" % room).send(
-        {
-            "text": json.dumps(dicRespText, sort_keys=True, indent=4),
-        }
-    )
-
-# Connected to websocket.disconnect
-@channel_session_user
-def ws_disconnect(message, room):
-    Group("chat-%s" % room).discard(message.reply_channel)
-    
 
 class ChatConsumer(JsonWebsocketConsumer):
     
