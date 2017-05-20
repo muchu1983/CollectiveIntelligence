1ff47019066eecbb75cbdde5346efd97b35d14a1
MuChu Hsu <muchu1983@gmail.com>
2017-05-21 01:40:30 +0800

    add paper.js content with init Quest button

--- ci/static/ci/js/canvas_main.paper.js
+++ ci/static/ci/js/canvas_main.paper.js
@@ -19,71 +19,164 @@ delete -- "new" -- apply --> "matching" -- accept --> "processing"
 $(document).ready(initCanvasMain);
 
 //全域變數
-var circleUser = null;
-var textUserA = null;
-var textUserB = null;
-var groupUserA = null;
-var groupUserB = null;
+var textUserLeft = null;
+var textUserRight = null;
+var groupQuest = null;
+var groupInitQuestLeft = null;
+var groupInitQuestRight = null;
 
 //初始化 #canvas_main 畫布
 function initCanvasMain() {
     //初始化 物件
     initCanvasItemObject();
+    //初始化 事件
+    initCanvasEvent();
 };
 
 //初始化 物件
 function initCanvasItemObject() {
-    //使用者圓
-    circleUser = new Path.Circle({
+    //用戶外框
+    rectUserLeft = new Path.Rectangle({
+        point: [0, 0],
+        size: [view.size.width/2-2, view.size.height],
+        radius: 10,
+        strokeColor: "red",
+        strokeWidth: 2,
+    });
+    rectUserRight = new Path.Rectangle({
+        point: [0, 0],
+        size: [view.size.width/2-2, view.size.height],
+        radius: 10,
+        strokeColor: "blue",
+        strokeWidth: 2,
+    });
+    
+    //用戶文字
+    textUserLeft = new PointText({
+        point: [view.size.width/4, 20],
+        justification: "center",
+        content: "A用戶",
+        fillColor: "black",
+        fontFamily: "sans-serif",
+        fontWeight: "normal",
+        fontSize: 18
+    });
+    textUserRight = new PointText({
+        point: [view.size.width/4, 20],
+        justification: "center",
+        content: "B用戶",
+        fillColor: "black",
+        fontFamily: "sans-serif",
+        fontWeight: "normal",
+        fontSize: 18
+    });
+    
+    //任務外框
+    circleQuest = new Path.Circle({
         center: view.center,
-        radius: 20,
-        fillColor: "white",
-        strokeWidth: 1,
-        strokeColor: "black"
+        radius: view.size.height/4,
+        strokeColor: "black",
+        fillColor: "gold",
+        strokeWidth: 2,
     });
-    var symbolCircleUser = new Symbol(circleUser);
-    
-    //使用者文字
-    textUserA = new PointText({
+    //任務文字
+    textQuest = new PointText({
         point: view.center,
         justification: "center",
-        content: "A",
-        fillColor: "black",
-        fontFamily: "sans-serif",
-        fontWeight: "normal",
-        fontSize: 18
-    });
-    textUserB = new PointText({
-        point: view.center,
-        justification: "center",
-        content: "B",
+        content: "任務",
         fillColor: "black",
         fontFamily: "sans-serif",
         fontWeight: "normal",
         fontSize: 18
     });
     
-    //群組 - 使用者
-    groupUserA = new Group({
-        children: [symbolCircleUser.place(view.center), textUserA],
-        position: [150,150],
+    //左發起任務外框
+    rectInitQuestLeft = new Path.Rectangle({
+        point: [0, 0],
+        size: [150, 30],
+        radius: 10,
+        fillColor: "red",
+        strokeColor: "black",
+        strokeWidth: 2,
+    });
+    //左發起任務文字
+    textInitQuestLeft = new PointText({
+        point: [rectInitQuestLeft.bounds.width/2, rectInitQuestLeft.bounds.height/2+3],
+        justification: "center",
+        content: "發起任務",
+        fillColor: "black",
+        fontFamily: "MingLiU",
+        fontWeight: "normal",
+        fontSize: 20
+    });
+    
+    //右發起任務外框
+    rectInitQuestRight = new Path.Rectangle({
+        point: [0, 0],
+        size: [150, 30],
+        radius: 10,
+        fillColor: "red",
+        strokeColor: "black",
+        strokeWidth: 2,
+    });
+    //右發起任務文字
+    textInitQuestRight = new PointText({
+        point: [rectInitQuestLeft.bounds.width/2, rectInitQuestLeft.bounds.height/2+3],
+        justification: "center",
+        content: "發起任務",
+        fillColor: "black",
+        fontFamily: "MingLiU",
+        fontWeight: "normal",
+        fontSize: 20
+    });
+    
+    //群組
+    groupUserLeft = new Group({
+        children: [rectUserLeft, textUserLeft],
+        position: [view.size.width*1/4, view.size.height/2],
+        visible: true,
+    });
+    groupUserRight = new Group({
+        children: [rectUserRight, textUserRight],
+        position: [view.size.width*3/4, view.size.height/2],
+        visible: true,
+    });
+    groupQuest = new Group({
+        children: [circleQuest, textQuest],
+        position: view.center,
         visible: false,
     });
-    groupUserB = new Group({
-        children: [symbolCircleUser.place(view.center), textUserB],
-        position: [300,150],
-        visible: false,
+    groupInitQuestLeft = new Group({
+        children: [rectInitQuestLeft, textInitQuestLeft],
+        position: [view.size.width*1/4, view.size.height/2],
+        visible: true,
     });
-    groupUserB.visible = true;
-    groupUserA.visible = true;
+    groupInitQuestRight = new Group({
+        children: [rectInitQuestRight, textInitQuestRight],
+        position: [view.size.width*3/4, view.size.height/2],
+        visible: true,
+    });
+    
 };
 
+//事件
+function initCanvasEvent() {
+    //左方按下發起任務
+    groupInitQuestLeft.onClick = function(){
+        groupQuest.visible = true;
+        groupInitQuestLeft.visible = false;
+        groupInitQuestRight.visible = false;
+        textUserLeft.content = "發起人"
+    };
+    //右方按下發起任務
+    groupInitQuestRight.onClick = function(){
+        groupQuest.visible = true;
+        groupInitQuestLeft.visible = false;
+        groupInitQuestRight.visible = false;
+        textUserRight.content = "發起人"
+    };
+};
 //動畫
 function onFrame(event) {
-    var intTimeSecond = Math.floor(event.time)
-    if (event.count % 10 == 0){
-        if(groupUserA){
-            groupUserA.visible = !groupUserA.visible;
-        }
-    };
+    
 };
