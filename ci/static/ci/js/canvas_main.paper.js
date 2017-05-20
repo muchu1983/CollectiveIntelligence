ff0dfdcd1100744fa59b5f2f3ec9bf408d8bddd2
MuChu Hsu <muchu1983@gmail.com>
2017-05-21 02:56:31 +0800

    add delete button paper.js

--- ci/static/ci/js/canvas_main.paper.js
+++ ci/static/ci/js/canvas_main.paper.js
@@ -22,10 +22,20 @@ $(document).ready(initCanvasMain);
 var textUserLeft = null;
 var textUserRight = null;
 var groupQuest = null;
-var groupInitQuestLeft = null;
-var groupInitQuestRight = null;
+
 var rectInitQuestLeft = null;
 var rectInitQuestRight = null;
+var textInitQuestLeft = null;
+var textInitQuestRight = null;
+var groupInitQuestLeft = null;
+var groupInitQuestRight = null;
+
+var rectDeleteQuestLeft = null;
+var rectDeleteQuestRight = null;
+var textDeleteQuestLeft = null;
+var textDeleteQuestRight = null;
+var groupDeleteQuestLeft = null;
+var groupDeleteQuestRight = null;
 
 //初始化 #canvas_main 畫布
 function initCanvasMain() {
@@ -83,7 +93,7 @@ function initCanvasItemObject() {
     });
     //任務文字
     textQuest = new PointText({
-        point: view.center,
+        point: view.center-[0, view.size.height/4-20],
         justification: "center",
         content: "任務",
         fillColor: "black",
@@ -132,6 +142,48 @@ function initCanvasItemObject() {
         fontSize: 20
     });
     
+    //左刪除任務外框
+    rectDeleteQuestLeft = new Path.Rectangle({
+        point: [0, 0],
+        size: [150, 30],
+        radius: 10,
+        fillColor: "pink",
+        strokeColor: "black",
+        strokeWidth: 2,
+    });
+    
+    //左刪除任務文字
+    textDeleteQuestLeft = new PointText({
+        point: [rectInitQuestLeft.bounds.width/2, rectInitQuestLeft.bounds.height/2+3],
+        justification: "center",
+        content: "刪除任務",
+        fillColor: "black",
+        fontFamily: "MingLiU",
+        fontWeight: "normal",
+        fontSize: 20
+    });
+    
+    //右刪除任務外框
+    rectDeleteQuestRight = new Path.Rectangle({
+        point: [0, 0],
+        size: [150, 30],
+        radius: 10,
+        fillColor: "pink",
+        strokeColor: "black",
+        strokeWidth: 2,
+    });
+    
+    //右刪除任務文字
+    textDeleteQuestRight = new PointText({
+        point: [rectInitQuestLeft.bounds.width/2, rectInitQuestLeft.bounds.height/2+3],
+        justification: "center",
+        content: "刪除任務",
+        fillColor: "black",
+        fontFamily: "MingLiU",
+        fontWeight: "normal",
+        fontSize: 20
+    });
+    
     //群組
     groupUserLeft = new Group({
         children: [rectUserLeft, textUserLeft],
@@ -158,42 +210,77 @@ function initCanvasItemObject() {
         position: [view.size.width*3/4, view.size.height/2],
         visible: true,
     });
-    
+    groupDeleteQuestLeft = new Group({
+        children: [rectDeleteQuestLeft, textDeleteQuestLeft],
+        position: [view.size.width*1/4, view.size.height/2],
+        visible: false,
+    });
+    groupDeleteQuestRight = new Group({
+        children: [rectDeleteQuestRight, textDeleteQuestRight],
+        position: [view.size.width*3/4, view.size.height/2],
+        visible: false,
+    });
 };
 
 //事件
 function initCanvasEvent() {
     //滑鼠進入按鈕
-    groupInitQuestLeft.onMouseEnter = function(event){
-        rectInitQuestLeft.fillColor = "red";
-    };
-    groupInitQuestRight.onMouseEnter = function(event){
-        rectInitQuestRight.fillColor = "red";
-    };
+    groupInitQuestLeft.onMouseEnter = onMouseEnterButtonItem(rectInitQuestLeft, textInitQuestLeft);
+    groupInitQuestRight.onMouseEnter = onMouseEnterButtonItem(rectInitQuestRight, textInitQuestRight);
+    groupDeleteQuestLeft.onMouseEnter = onMouseEnterButtonItem(rectDeleteQuestLeft, textDeleteQuestLeft);
+    groupDeleteQuestRight.onMouseEnter = onMouseEnterButtonItem(rectDeleteQuestRight, textDeleteQuestRight);
     //滑鼠離開按鈕
-    groupInitQuestLeft.onMouseLeave = function(event){
-        rectInitQuestLeft.fillColor = "pink";
-    };
-    groupInitQuestRight.onMouseLeave = function(event){
-        rectInitQuestRight.fillColor = "pink";
-    };
+    groupInitQuestLeft.onMouseLeave = onMouseLeaveButtonItem(rectInitQuestLeft, textInitQuestLeft);
+    groupInitQuestRight.onMouseLeave = onMouseLeaveButtonItem(rectInitQuestRight, textInitQuestRight);
+    groupDeleteQuestLeft.onMouseLeave = onMouseLeaveButtonItem(rectDeleteQuestLeft, textDeleteQuestLeft);
+    groupDeleteQuestRight.onMouseLeave = onMouseLeaveButtonItem(rectDeleteQuestRight, textDeleteQuestRight);
     //左方按下發起任務
     groupInitQuestLeft.onClick = function(event){
         groupQuest.visible = true;
         groupInitQuestLeft.visible = false;
         groupInitQuestRight.visible = false;
-        textUserLeft.content = "發起人"
+        textUserLeft.content = "發起人-A用戶"
+        groupDeleteQuestLeft.visible = true;
     };
     //右方按下發起任務
     groupInitQuestRight.onClick = function(event){
         groupQuest.visible = true;
         groupInitQuestLeft.visible = false;
         groupInitQuestRight.visible = false;
-        textUserRight.content = "發起人"
+        textUserRight.content = "發起人-B用戶"
+        groupDeleteQuestRight.visible = true;
+    };
+    //左方按下刪除任務
+    groupDeleteQuestLeft.onClick = function(event){
+        groupQuest.visible = false;
+        groupInitQuestLeft.visible = true;
+        groupInitQuestRight.visible = true;
+        textUserLeft.content = "A用戶"
+        groupDeleteQuestLeft.visible = false;
+    };
+    //右方按下刪除任務
+    groupDeleteQuestRight.onClick = function(event){
+        groupQuest.visible = false;
+        groupInitQuestLeft.visible = true;
+        groupInitQuestRight.visible = true;
+        textUserLeft.content = "B用戶"
+        groupDeleteQuestRight.visible = false;
     };
 };
 
-//動畫
-function onFrame(event) {
-    
+//滑鼠進入按鈕
+function onMouseEnterButtonItem(bgItem, textItem){
+    function wrapper(event){
+        bgItem.fillColor = "red";
+        textItem.fillColor = "white";
+    }
+    return wrapper;
+};
+//滑鼠離開按鈕
+function onMouseLeaveButtonItem(bgItem, textItem){
+    function wrapper(event){
+        bgItem.fillColor = "pink";
+        textItem.fillColor = "black";
+    }
+    return wrapper;
 };
