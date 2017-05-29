491730ce9973eabc01cc264c6b98c4c0d87b3cb7
MuChu Hsu <muchu1983@gmail.com>
2017-05-30 07:43:15 +0800

    add tinymce langs/zh_TW.js configure tinymce to looks excellent

--- ci/static/ci/js/initNewQuest.js
+++ ci/static/ci/js/initNewQuest.js
@@ -27,7 +27,20 @@
     function initTinymce(){
         tinymce.init({
             selector:"textarea",
-            width: "46vw"
+            language: "zh_TW",
+            width: "46vw",
+            menubar: false,
+            plugins: [
+                "advlist autolink lists link image charmap print preview anchor",
+                "searchreplace visualblocks code fullscreen",
+                "insertdatetime media table contextmenu paste code"
+            ],
+            toolbar: "undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+            content_css: [
+                "//fonts.googleapis.com/css?family=Lato:300,300i,400,400i",
+                "//www.tinymce.com/css/codepen.min.css"
+            ],
+            default_link_target: "_blank"
         });
     };
     
