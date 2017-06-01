656adb231df5b4cb66d77379aa232a0d894d3d37
MuChu Hsu <muchu1983@gmail.com>
2017-06-02 03:59:05 +0800

    add Google Analytics

--- ci/static/ci/js/base.js
+++ ci/static/ci/js/base.js
@@ -9,10 +9,29 @@
     $(document).ready(initBase);
     
     function initBase() {
+        initGA();
         initTooltip();
         //retrieveLstDicFollower();
         initBtnClearLeader();
         initBtnCloseQuestViewerIFrame();
+    };
+    
+    //初始化 Google Analytics
+    function initGA(){
+        (function(i,s,o,g,r,a,m){
+            i['GoogleAnalyticsObject']=r;
+            i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)
+            },
+            i[r].l=1*new Date();
+            a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];
+            a.async=1;
+            a.src=g;
+            m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-7021205-3', 'auto');
+        ga('send', 'pageview');
     };
     
     //初始化 tooltip
